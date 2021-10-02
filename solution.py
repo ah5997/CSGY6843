31cca1a89bcc6329e9c5bb43f3e26fea9ece30c4
ArzooHussian <ah5997@nyu.edu>
2021-10-02 14:28:28 -0400

    Python Programming: 17 check

--- solution.py
+++ solution.py
@@ -62,6 +62,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     messagedata = 'MESSAGE\r\n'
     #messagedata = 'message data'
     clientSocket.send(messagedata)
+    #clientSocket.send(msg)
     # recv5 = clientSocket.recv(1024).decode()
     # print(recv5)
     #  if recv5[:3] != '250':
